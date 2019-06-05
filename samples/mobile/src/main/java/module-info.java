f2001becf909409356c31197413184dfaa4c56c3
Johan Vos <johan.vos@gluonhq.com>
2019-06-05 17:08:47 +0200

    Jdk11 (#32)
    * new release: 1.0.3
    
    * new development version: 1.0.4-SNAPSHOT
    
    * #27 Adapt maps to Java 11 (#28)
    
    * #27 Adapt maps to Java 11
    - Move maps project to subproject
    - Use javafx plugin
    - Create modules for maps and sample
    
    * #27 Update travis
    
    * #28 Remove private repos
    
    * #27 Don’t publish JavaFX classifier
    
    * Update README.md
    
    * fix dependencies

---
+++ samples/mobile/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.gluonhq.maps.sample {
+    requires javafx.controls;
+    requires com.gluonhq.maps;
+    requires com.gluonhq.attach.position;
+    requires com.gluonhq.attach.util;
+    requires java.logging;
+
+    exports com.gluonhq.maps.samples.mobile;
+}
