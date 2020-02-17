ad42bb45ed57d2a5483f7a2a2090c32d7fb9a144
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-02-17 17:06:28 +0530

    Position plugin has a dependency on lifecycle plugin (#38)

--- samples/mobile/src/main/java/module-info.java
+++ samples/mobile/src/main/java/module-info.java
@@ -29,6 +29,7 @@ module com.gluonhq.maps.sample {
     requires javafx.controls;
     requires com.gluonhq.maps;
     requires com.gluonhq.attach.position;
+    requires com.gluonhq.attach.lifecycle;
     requires com.gluonhq.attach.util;
     requires java.logging;
 
